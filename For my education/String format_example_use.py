dba199c8544944ac183f5c84ba5bba17c4642cb9
Faiz <faiz1992@mail.ru>
2021-03-19 17:54:58 +0300

    Обновлен файл Работы со строками добавленно описание аналога Тернарного оператора Добавлен пример работы с Assert в Seleniume

--- For my education/String format_example_use.py
+++ For my education/String format_example_use.py
@@ -9,5 +9,34 @@ print("My name is {name}. I\'m {age}".format(name=name, age=age))
 
 #   f-strings
 print(f"My name is {name}. I\'m {age}")
-print(f'My name is {name}. I\'m {age+5}')
-print('5 + 2 = {}'.format(5 + 2))
+print(f'My name is {name}. I\'m {age + 5}')
+#print('5 + 2 = {}'.format(5 + 2))     при помощи format
+print(f'5 + 2 = {5 + 2}')
+
+#                       Работа со строками
+s = 'Py' 'Thon'
+r = 'wdwa'
+print(s, r, end=' ')
+print(s)
+s = 'Answer'
+print(s[::-2])
+
+print("Price: %.3f , Name brand: %s" % (40, "Samsung"))
+
+#                       Условия if
+
+x = 0
+if not x:
+    print("OK")
+else:
+    print("NO")
+
+# Тернарный оператор или то же что и выше но короче
+
+x = 1
+res = 'OK' if x else 'No'
+print(res)
+
+# или можно так
+
+print('OK' if x else 'No')
