52fe538ea31e608fbc860a479d1a0c198b9fcfe3
Faiz <faiz1992@mail.ru>
2021-03-21 11:49:18 +0300

    Добавлен файл образец видов генерации списко и способо работы со списками

---
+++ For my education/Таблица умножения.py
@@ -0,0 +1,10 @@
+# for i in range(1, 10):
+#     print()
+#     for j in range(1, 10):
+#         print(i * j, end=' ')
+
+
+for i in range(1, 11):
+    print()
+    for j in range(1, 11):
+        print(f'{i} * {j} = {i * j}')
