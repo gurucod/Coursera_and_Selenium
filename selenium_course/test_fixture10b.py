91ffc24c28e795f9419a2265ee2b1674a732091c
Faiz <faiz1992@mail.ru>
2021-03-22 20:22:34 +0300

    марка xfail становится xpass т.е. баг исправлен и тест проходит

---
+++ selenium_course/test_fixture10b.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+class TestMainPage1():
+
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+    @pytest.mark.xfail(reason="fixing this bug right now")
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("input.btn.btn-default")
+
+
+# Тут кнопка найдена и марка xfail становится xpass т.е. баг исправлен и тест проходит
+#   pytest -rX -v test_fixture10b.py
+# Здесь мы добавили символ X в параметр -r, чтобы получить подробную информацию по XPASS-тестам:
