1a43235d2aa1f1386a4f37fefcef03b14450bc2a
faiz <faiz1992@mail.ru>
2023-12-13 14:31:44 +0100

    Убрал лишнее

--- selenium_course/test_conftest.py
+++
@@ -1,9 +0,0 @@
-import time
-
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-def test_guest_should_see_login_link(browser):
-    browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
-    time.sleep(10)
